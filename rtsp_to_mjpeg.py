c353ddd49fedcd90b2245f9c62a34a76d17d8489
Jaturong Sarak <jaturongsarak.ball@gmail.com>
2025-02-28 21:58:40 +0700

    update

--- rtsp_to_mjpeg.py
+++ rtsp_to_mjpeg.py
@@ -6,25 +6,32 @@ from flask import Flask, Response, request
 app = Flask(__name__)
 
 os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = (
-    "rtsp_transport;tcp|timeout;8000|max_delay;800000|fflags;nobuffer|flags;low_delay"
-    "|probesize;500000|analyzeduration;250000|thread_queue_size;4096|framedrop;0|skip_frame;default"
-    "|flags2;showall|sync;ext|avioflags;direct|reorder_queue_size;16"
+    "rtsp_transport;tcp|timeout;5000|max_delay;0|fflags;nobuffer|flags;low_delay"
+    "|probesize;1000000|analyzeduration;500000|thread_queue_size;1024|framedrop;1|skip_frame;nonref"
+    "|flags2;showall|sync;ext|avioflags;direct|reorder_queue_size;0"
 )
 
 def generate_frames(rtsp_url):
-    cap = cv2.VideoCapture(rtsp_url)
+    cap = cv2.VideoCapture(rtsp_url, cv2.CAP_FFMPEG)
     if not cap.isOpened():
         print(f"❌ ไม่สามารถเชื่อมต่อ RTSP: {rtsp_url}")
         return
     else:
         print(f"✅ เชื่อมต่อกล้องสำเร็จ")
+    
+    target_fps = 30
+    frame_time = 1.0 / target_fps
+
 
     while True:
+
+        start_time = time.time()
+
         success, frame = cap.read()
         if not success:
             print("⚠️ ไม่สามารถอ่านภาพจาก RTSP stream ได้ กำลังพยายามเชื่อมต่อใหม่...")
             cap.release()
-            cap = cv2.VideoCapture(rtsp_url)
+            cap = cv2.VideoCapture(rtsp_url, cv2.CAP_FFMPEG)
             if not cap.isOpened():
                 print(f"❌ ไม่สามารถเชื่อมต่อ RTSP: {rtsp_url}")
             else:
@@ -36,6 +43,10 @@ def generate_frames(rtsp_url):
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        
+        elapsed_time = time.time() - start_time
+        sleep_time = max(0, frame_time - elapsed_time)
+        time.sleep(sleep_time)
 
 @app.route('/', methods=['GET'])
 def mjpeg_feed():
